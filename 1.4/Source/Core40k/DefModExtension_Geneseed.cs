54960e443772f0f7150aac5d2a5f6f7672c94f28
Phonicmas <jkskovlund@gmail.com>
2023-08-25 20:48:20 +0200

    Patch 25/8

---
+++ 1.4/Source/Core40k/DefModExtension_Geneseed.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Core40k
+{
+    public class DefModExtension_Geneseed : DefModExtension
+    {
+        public List<GeneDef> givesGenes;
+    }
+
+}   
---
+++ 1.4/Source/Core40k/RecipeWorkerClass_Geneseed.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Core40k
+{
+    public class RecipeWorkerClass_Geneseed : RecipeWorker
+    {
+        public override void Notify_IterationCompleted(Pawn billDoer, List<Thing> ingredients)
+        {
+            if (recipe.GetModExtension<DefModExtension_Geneseed>() == null)
+            {
+                return;
+            }
+            List<GeneDef> genesToGive = recipe.GetModExtension<DefModExtension_Geneseed>().givesGenes;
+
+            if (billDoer.genes != null)
+            {
+                //Adds genes to give
+                if (!genesToGive.NullOrEmpty())
+                {
+                    foreach (GeneDef gene in genesToGive)
+                    {
+                        if (gene != null && !billDoer.genes.HasGene(gene))
+                        {
+                            billDoer.genes.AddGene(gene, true);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
